fb7c570dbf984b12bbce3c657ac40d5a0ae2de57
ProsperieEli <ElijahProsperie@gmail.com>
2021-11-01 19:45:22 -0400

    finished SimpleDb

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const app = require('./lib/app.js');
-const pool = require('./lib/utils/pool.js');
+const http = require('http');
 
 const PORT = process.env.PORT || 7890;
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Started on ${PORT}`);
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+
 });
 
-process.on('exit', () => {
-  console.log('Goodbye!');
-  pool.end();
-});
